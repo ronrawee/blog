e8a776f195f247abf60e8d5b2f6258d196586557
ronrawee <ronrawee@hotmail.com>
2016-11-26 17:56:43 +1100

    slug inprogress

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,72 @@
+@extends('main')
+
+@section('title', '| View Post')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-8">
+        <h1>{{ $post->title }}</h1>
+
+        <p class="lead">{{ $post->body }}</p>
+
+    </div>
+
+    <div class="col-md-4">
+        <div class="well">
+            
+            <dl class="dl-horizontal">
+                <dt>Url:</dt>
+                <dd><a href="{{ url($post->slug) }}">{{ url($post->slug) }}</a></dd>
+            </dl>
+            
+            <dl class="dl-horizontal">
+                <dt>Created At:</dt>
+                <dd>{{ date( 'M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+            </dl>
+
+            <dl class="dl-horizontal">
+                <dt>Last Updated At:</dt>
+                <dd>{{ date( 'M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+            </dl>
+
+            <hr>
+
+            <div class="row">
+                <div class="col-md-6">
+
+                    {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-warning btn-block')) !!}
+
+                </div>
+                <div class="col-md-6">
+
+                    {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'DELETE']) !!}
+
+                    {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-block'])}}
+
+                    {!! Form::close() !!}
+
+
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    {!! Html::linkRoute('posts.index', '<< See All Posts', [], ['class' => 'btn btn-default btn-block see-all']) !!}
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+
+
+@endsection
+
